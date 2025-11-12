8c68e269a9fcc3bcf2c7581d754288a36ee2f1f8
aryanvib <aryanvib@terpmail.umd.edu>
2025-11-12 23:25:34 +0000

    Deploying to gh-pages from @ aryanvib/aryanvib.github.io@f5ffc54f798b3cacee4c770b20710b8ffc6f097b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here&#39;s a quick look at what I&#39;ve worked on in the past.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
